961684e1d71176d8ee134e578352a853b590691e
Rahul Dubey <rahuldubey7398@gmail.com>
2026-01-27 10:35:51 +0530

    no params error

--- src/main/java/com/logicule/books/exception/BookNotFoundException.java
+++ src/main/java/com/logicule/books/exception/BookNotFoundException.java
@@ -4,6 +4,9 @@ public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException(String message) {
         super(message);
     }
+    public BookNotFoundException() {
+        super("Error");
+    }
 
     public BookNotFoundException(String message, Throwable cause) {
         super(message, cause);
